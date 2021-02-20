93debb471b51db47baad1658bb980b1b07389e40
aait <info@aait.se>
2021-02-20 16:54:50 +0600

    Update Consumer

--- tests/unit/ConsumerTest.php
+++ tests/unit/ConsumerTest.php
@@ -6,11 +6,11 @@ class ConsumerTest extends TestCase
 {
     public function testInitiateConsumerSession()
     {
-        $result = $this->core->initiateConsumerSession('SE');
+        $result = $this->core->initiateConsumerSession('sv-SE', true, ['NO', 'SE', 'DK']);
         $this->assertInstanceOf(Response::class, $result);
         $this->assertArrayHasKey('token', $result);
         $this->assertArrayHasKey('operations', $result);
-        $this->assertIsString($result->getOperationByRel('redirect-consumer-identification'));
+        $this->assertIsString($result->getOperationByRel('redirect-consumer-identification')); // deprecated
         $this->assertIsString($result->getOperationByRel('view-consumer-identification'));
     }
 }
